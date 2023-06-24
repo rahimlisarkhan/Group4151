2d1962aada554c9b3a71c2377c3a1e9c3b88aa16
rahimlisarkhan <whelp@Whelps-MacBook-Air.local>
2023-06-24 12:39:41 +0400

    update

---
+++ Fundamentals/Topic6/array.js
@@ -0,0 +1,103 @@
+// var a = 5;
+
+// var b = "hello";
+
+// var c = false;
+
+// var d = 4;
+
+// var arr = [
+//   5,
+//   "hello",
+//   false,
+//   4,
+//   null,
+//   5.4,
+//   undefined,
+//   [99, 87, 89, ["say", "soy"]],
+// ];
+
+// var e = arr[7][1];
+
+// arr[7][1] = "hi";
+// arr[6] = "huy";
+
+// console.log("e:", e);
+
+// console.log("arr:", arr);
+
+// console.log(arr[7][3][0]);
+
+// var infoText = "fruits: ";
+
+// var fruits = ["Apple", "Pie", "Mango"];
+
+// fruits[1] = "Watermaloon";
+
+// fruits[3] = "Pineapple";
+
+// var singleFruit = fruits[1];
+
+// fruits[1] = "Pie";
+
+// console.log('"singleFruit', singleFruit);
+
+// // console.log(infoText, fruits);
+
+// var a = 5;
+
+// a = 10;
+
+// a = 20;
+
+var fruits = ["Mango", "Avakado"];
+var arr = ["Hello", "Hi", "Bye", "Good", "ayten"];
+
+// arr[0] = "Test1";
+// arr[1] = "Test2";
+
+// arr.push(1);
+// arr.push(5);
+
+// var a = arr.unshift("Test4", "test5");
+// var a = arr.push("Test4");
+// arr.unshift(5);
+
+// var a = arr.pop();
+// var a = arr.shift();
+
+// var a = arr.indexOf("Hello");
+// var b = arr.includes("Hello");
+
+// var lastEl = arr.length - 1;
+
+// var newArr = arr.concat(fruits);
+
+// arr.reverse();
+// var text = arr.join(" ");
+
+// console.log(arr[lastEl]);
+
+// var text = "Vuqar";
+
+// var textArr = text.split("");
+
+// if (textArr.includes("q")) {
+//   console.log("Gel sinfe");
+// } else {
+//   console.log("Get adivi deyis");
+// }
+
+// console.log(textArr);
+
+var arr = [];
+
+var hansiEded = prompt("Hansi eded daxil olunsun?");
+
+arr.push(hansiEded);
+
+arr.push("");
+
+console.log(arr);
+
+// console.log("text", text[2]);
