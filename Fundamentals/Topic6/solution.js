31012d919dab818b2b8fbea736a533c113046b28
rahimlisarkhan <whelp@Whelps-MacBook-Air.local>
2023-07-04 19:06:30 +0400

    updatE

---
+++ Fundamentals/Topic6/solution.js
@@ -0,0 +1,11 @@
+//? 1. Array empty
+//? 2. Prompt question for loop length
+//? 3. You able to create "for loop" for invite question.
+//? 4. We will fill array for log.
+
+var people = [];
+var inviteCount = ""; //promt
+
+//! for
+
+//! for
