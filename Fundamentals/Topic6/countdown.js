cd1ac14f2077e6ede3b2ed66e3f07a2afba08c71
rahimlisarkhan <whelp@Whelps-MacBook-Air.local>
2023-07-13 12:40:40 +0400

    update

--- Fundamentals/Topic6/countdown.js
+++ Fundamentals/Topic6/countdown.js
@@ -11,13 +11,29 @@ function getCoundownTime(userTime) {
   return days;
 }
 
-var countDownDays = getCoundownTime(userDate);
+// var countDownDays = getCoundownTime(userDate);
 
-console.log(countDownDays);
+// console.log(countDownDays);
 
-var employeTime = new Date();
-var currentDate = new Date();
+// var employeTime = new Date();
+// var currentDate = new Date();
 
-employeTime.setFullYear(2020, 11, 23);
+// employeTime.setFullYear(2020, 11, 23);
 
-employeTime.getTime();
+// employeTime.getTime();
+
+// var time = "03/25/2015";
+
+// var convert = time.split("/");
+// ["03", "25", "2015"];
+
+// var azerbaijaniDateFormat = `${convert[1]}/${convert[0]}/${convert[2]}`;
+
+var time = "25/03/2015";
+
+var convert = time.split("/");
+// ["25", "03", "2015"];
+
+var azerbaijaniDateFormat = `${convert[1]}/${convert[0]}/${convert[2]}`;
+
+var futureDatatime = new Date(azerbaijaniDateFormat);
