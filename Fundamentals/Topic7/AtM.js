d238539eeeca8855205c2a4c2df8c891ce836376
rahimlisarkhan <whelp@Whelps-MacBook-Air.local>
2023-08-19 12:36:07 +0400

    update

--- Fundamentals/Topic7/AtM.js
+++ Fundamentals/Topic7/AtM.js
@@ -1,73 +1,161 @@
-class DomSelector {
-  regestration_form = document.querySelector("#regestration_form");
-  addMany = document.querySelector("#add_many");
-  payMany = document.querySelector("#pay_many");
-  showBalance = document.querySelector("#show_balance");
-  err_place = document.querySelector("#err_message");
-  my_balance = document.querySelector("#my_balance");
-  noShow = document.querySelector(".noShow");
+// class DomSelector {
+//   regestration_form = document.querySelector("#regestration_form");
+//   addMany = document.querySelector("#add_many");
+//   payMany = document.querySelector("#pay_many");
+//   showBalance = document.querySelector("#show_balance");
+//   err_place = document.querySelector("#err_message");
+//   my_balance = document.querySelector("#my_balance");
+//   noShow = document.querySelector(".noShow");
 
-  showDom() {
-    console.log("Fill dom element");
+//   showDom() {
+//     console.log("Fill dom element");
+//   }
+// }
+
+// class BankAcount extends DomSelector {
+//   balance = 0;
+//   #userName = null;
+//   cur_balance = null;
+
+//   constructor(fullname, balance) {
+//     super();
+
+//     this.balance = balance;
+//     this.#userName = fullname;
+//   }
+
+//   getRegister(name, age, event) {
+//     if (!(name == "" || age == "")) {
+//       event.target.style.display = "none";
+//       this.noShow.style.display = "flex";
+//       this.#userName = name;
+
+//       this.monitor();
+//     }
+//     //   showErr(true, err_place, "Xahis edirik adivizi veya yasinizi girin");
+//   }
+
+//   get username() {
+//     return this.#userName;
+//   }
+
+//   set username(u) {
+//     if (u != "") {
+//       this.#userName = u;
+//     }
+//   }
+
+//   monitor() {
+//     console.log("Hello");
+//   }
+
+//   increment(price) {
+//     if (price >= 0) {
+//       this.my_balance += price;
+//       showBalance.innerHTML = "Balansivizi yenileyin";
+//     } else {
+//       alert("sizin qiymetiviz musbet eded olmalidir");
+//     }
+//   }
+// }
+
+// const customerIzzetM = new BankAcount("Izzet Memmedov", 200);
+
+// console.log(customerIzzetM);
+
+// customerIzzetM.regestration_form.addEventListener("submit", function (event) {
+//   event.preventDefault();
+//   let name = this.name_input.value;
+//   let age = this.age_input.value;
+
+//   customerIzzetM.getRegister(name, age, event);
+// });
+
+// customerIzzetM.addMany.addEventListener("click", function () {
+//   let price = +prompt("Balansinizi artiramaq ucun qiymet daxil edin.");
+//   customerIzzetM.increment(price);
+// });
+
+// const mirzeSBMW = new Bmw("Mirze", 30000);
+
+class Car {
+  carSpeed = 0;
+  carLights = false;
+  carFuel = 0;
+
+  constructor(carType, carFuel) {
+    this.carType = carType;
+    this.carFuel = carFuel;
   }
 }
 
-class BankAcount extends DomSelector {
-  balance = 0;
-  #userName = null;
-  cur_balance = null;
+// const car = new Car("Sedan", 30000);
 
-  constructor(fullname, balance) {
-    super();
+class BMW extends Car {
+  carName = "BMW";
 
-    this.balance = balance;
-    this.#userName = fullname;
+  constructor(carType, carPrice, carFuel) {
+    super(carType, carFuel);
+    this.carPrice = carPrice;
   }
 
-  getRegister(name, age, event) {
-    if (!(name == "" || age == "")) {
-      event.target.style.display = "none";
-      this.noShow.style.display = "flex";
-      this.#userName = name;
-    }
-    //   showErr(true, err_place, "Xahis edirik adivizi veya yasinizi girin");
-  }
+  // constructor(carType, carPrice, carFuel) {
+  //   super();
+  //   this.carType = carType;
+  //   this.carPrice = carPrice;
+  //   this.carFuel = carFuel;
+  // }
 
-  get username() {
-    return this.#userName;
-  }
-
-  set username(u) {
-    if (u != "") {
-      this.#userName = u;
-    }
-  }
-
-  increment(price) {
-    if (price >= 0) {
-      this.my_balance += price;
-      showBalance.innerHTML = "Balansivizi yenileyin";
+  drive() {
+    if (this.carFuel > 0) {
+      this.carLights = true;
+      this.carPrice++;
+      this.carFuel--;
+      console.log("Driving");
     } else {
-      alert("sizin qiymetiviz musbet eded olmalidir");
+      console.log("no gas");
     }
+  }
+  stop() {
+    this.carLights = false;
   }
 }
 
-const customerIzzetM = new BankAcount("Izzet Memmedov", 200);
+let myCar = new BMW("sedan", 30000, 100);
 
-console.log(customerIzzetM);
+console.log(myCar);
 
-customerIzzetM.regestration_form.addEventListener("submit", function (event) {
-  event.preventDefault();
-  let name = this.name_input.value;
-  let age = this.age_input.value;
+/**
+ *
+ * @param {string} customerCode müştəri kodu
+ * @param {number} beginninMeter sayğacın başlanğıc göstəricisi
+ * @param {number} endingMeter sayğacın son göstəricisi
+ * @param {number} totalUsage gallon ilə su miqdarı
+ * @param {number} bill borc
+ *
+ * @returns {string} səliqəli mesaj hazırlayıb qaytarır
+ */
+const getAccurateMessage = (
+  customerCode,
+  beginninMeter,
+  endingMeter,
+  totalUsage,
+  bill
+) => {
+  return `
+  Customer code: ${customerCode}
+  Beginning meter reading: ${beginninMeter}
+  Ending meter reading: ${endingMeter}
+  Gallons of water used: ${totalUsage} gallon
+  Amount billed: $${bill}
+  `;
+};
 
-  customerIzzetM.getRegister(name, age, event);
-});
 
-customerIzzetM.addMany.addEventListener("click", function () {
-  let price = +prompt("Balansinizi artiramaq ucun qiymet daxil edin.");
-  customerIzzetM.increment(price);
-});
 
-const mirzeSBMW = new Bmw("Mirze", 30000);
+/**
+@param {number} num1
+@param {number} num2
+**/
+
+function sum(num1,num2)
--- Fundamentals/Topic7/json.js
+++ Fundamentals/Topic7/json.js
@@ -31,3 +31,152 @@ el.innerHTML = result;
 // const result = student["1bornIn"];
 
 console.log(result);
+
+const restuarants = [
+  {
+    name: "Papa John's",
+    address: [
+      { country: "Spain", street: "" },
+      { country: "Azerbaijan", street: "Nizami 45" },
+    ],
+  },
+  {
+    name: "Mc Donald's",
+    address: [
+      { country: "Spain", street: "" },
+      { country: "Azerbaijan", street: "Nizami 45" },
+    ],
+  },
+];
+
+var myArray = [
+  {
+    name: "chicken pizza",
+    vegan: false,
+    oil: true,
+    cheese: true,
+    baked: true,
+    fish: false,
+    rice: false,
+  },
+  {
+    name: "sushi",
+    vegan: true,
+    oil: false,
+    cheese: false,
+    baked: false,
+    fish: true,
+    rice: true,
+  },
+  {
+    name: "Xan pulovu",
+    vegan: false,
+    oil: true,
+    cheese: false,
+    baked: true,
+    fish: false,
+    rice: true,
+  },
+  {
+    name: "paxlalı pulovu",
+    vegan: true,
+    oil: true,
+    cheese: false,
+    baked: true,
+    fish: false,
+    rice: true,
+  },
+];
+
+const person = {
+  fullname: "Izzet Memmedov",
+  age: 35,
+  marriage: true,
+  children: 2,
+  job: "Developer",
+  salary: 15000,
+  cars: [
+    { name: "Ford", models: ["Fiesta", "Focus", "Mustang"] },
+    { name: "BMW", models: ["320", "X3", "X5"] },
+    { name: "Fiat", models: ["500", "Panda"] },
+  ],
+  hobbies: [{ name: "Coding" }, { name: "Fitness" }, { name: "Swimming" }],
+
+  wife: [{ name: "X", age: 28, job: "Designer", sallary: 2000 }],
+  children: [
+    { name: "X", age: 4 },
+    { name: "X", age: 7 },
+    { name: "X", age: 1 },
+  ],
+};
+
+console.log(person.cars);
+console.log(person.cars[1].models[2]);
+console.log(person.wife[0]);
+console.log(person.children[0]);
+
+const people = [
+  {
+    fullname: "Izzet Memmedov",
+    age: 35,
+    marriage: true,
+    children: 2,
+    job: "Developer",
+    salary: 15000,
+    cars: [
+      { name: "Ford", models: ["Fiesta", "Focus", "Mustang"] },
+      { name: "BMW", models: ["320", "X3", "X5"] },
+      { name: "Fiat", models: ["500", "Panda"] },
+    ],
+    hobbies: [{ name: "Coding" }, { name: "Fitness" }, { name: "Swimming" }],
+
+    wife: [{ name: "X", age: 28, job: "Designer", sallary: 2000 }],
+    children: [
+      { name: "X", age: 4 },
+      { name: "X", age: 7 },
+      { name: "X", age: 1 },
+    ],
+  },
+  {
+    fullname: "Kenan Memmedov",
+    age: 35,
+    marriage: true,
+    children: 2,
+    job: "Developer",
+    salary: 15000,
+    cars: [
+      { name: "Ford", models: ["Fiesta", "Focus", "Mustang"] },
+      { name: "BMW", models: ["320", "X3", "X5"] },
+      { name: "Fiat", models: ["500", "Panda"] },
+    ],
+    hobbies: [{ name: "Coding" }, { name: "Fitness" }, { name: "Swimming" }],
+
+    wife: [{ name: "X", age: 28, job: "Designer", sallary: 2000 }],
+    children: [
+      { name: "X", age: 4 },
+      { name: "X", age: 7 },
+      { name: "X", age: 1 },
+    ],
+  },
+  {
+    fullname: "Xalide Memmedova",
+    age: 35,
+    marriage: true,
+    children: 2,
+    job: "Developer",
+    salary: 15000,
+    cars: [
+      { name: "Ford", models: ["Fiesta", "Focus", "Mustang"] },
+      { name: "BMW", models: ["320", "X3", "X5"] },
+      { name: "Fiat", models: ["500", "Panda"] },
+    ],
+    hobbies: [{ name: "Coding" }, { name: "Fitness" }, { name: "Swimming" }],
+
+    wife: [{ name: "X", age: 28, job: "Designer", sallary: 2000 }],
+    children: [
+      { name: "X", age: 4 },
+      { name: "X", age: 7 },
+      { name: "X", age: 1 },
+    ],
+  },
+];
