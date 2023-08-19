11f646065c8795e54768170e846750e0df911b89
rahimlisarkhan <whelp@Whelps-MacBook-Air.local>
2023-08-19 11:19:07 +0400

    update

---
+++ Fundamentals/Topic6/AtM.js
@@ -0,0 +1,71 @@
+class DomSelector {
+  regestration_form = document.querySelector("#regestration_form");
+  addMany = document.querySelector("#add_many");
+  payMany = document.querySelector("#pay_many");
+  showBalance = document.querySelector("#show_balance");
+  err_place = document.querySelector("#err_message");
+  my_balance = document.querySelector("#my_balance");
+  noShow = document.querySelector(".noShow");
+
+  showDom() {
+    console.log("Fill dom element");
+  }
+}
+
+class BankAcount extends DomSelector {
+  balance = 0;
+  #userName = null;
+  cur_balance = null;
+
+  constructor(fullname, balance) {
+    super();
+
+    this.balance = balance;
+    this.#userName = fullname;
+  }
+
+  getRegister(name, age, event) {
+    if (!(name == "" || age == "")) {
+      event.target.style.display = "none";
+      this.noShow.style.display = "flex";
+      this.#userName = name;
+    }
+    //   showErr(true, err_place, "Xahis edirik adivizi veya yasinizi girin");
+  }
+
+  get username() {
+    return this.#userName;
+  }
+
+  set username(u) {
+    if (u != "") {
+      this.#userName = u;
+    }
+  }
+
+  increment(price) {
+    if (price >= 0) {
+      this.my_balance += price;
+      showBalance.innerHTML = "Balansivizi yenileyin";
+    } else {
+      alert("sizin qiymetiviz musbet eded olmalidir");
+    }
+  }
+}
+
+const customerIzzetM = new BankAcount("Izzet Memmedov", 200);
+
+console.log(customerIzzetM);
+
+customerIzzetM.regestration_form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let name = this.name_input.value;
+  let age = this.age_input.value;
+
+  customerIzzetM.getRegister(name, age, event);
+});
+
+customerIzzetM.addMany.addEventListener("click", function () {
+  let price = +prompt("Balansinizi artiramaq ucun qiymet daxil edin.");
+  customerIzzetM.increment(price);
+});
--- Fundamentals/Topic6/events.js
+++ Fundamentals/Topic6/events.js
@@ -68,3 +68,75 @@ for (let i = 0; i < firstArr.length; i++) {
 //   rmvBalance();
 //   return;
 // }
+
+const data = {
+  name: "Bestteller",
+  children: ["#dasdsa", "#dasdasdsa"],
+};
+
+const atmData = {
+  my_balance: 0,
+  userName: "",
+  cur_balance: null,
+  showBalance: document.querySelector("#showBalance"),
+
+  increment(price) {
+    this.my_balance += price;
+    this.showBalance.innerHTML = "Balansivizi yenileyin";
+  },
+
+  decrement(price) {
+    if (this.my_balance <= 0 || this.my_balance - price < 0) {
+      alert("Balansivizi yoxlayin");
+    } else {
+      this.my_balance -= price;
+    }
+    this.showBalance.innerHTML = "Balansivizi yenileyin";
+  },
+
+  getInfo(name, cur) {
+    this.userName = name;
+    this.cur_balance = cur;
+  },
+
+  balance() {
+    this.showBalance.innerHTML = "Balans gosterildi";
+    showErr(true, this.cur_balance, this.my_balance);
+  },
+};
+
+regestration_form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let name = this.name_input.value;
+  let age = this.age_input.value;
+
+  if (!(name == "" || age == "")) {
+    event.target.style.display = "none";
+    document.querySelector(".noShow").style.display = "flex";
+
+    atmData.getInfo(name, document.querySelector("#my_balance"));
+  }
+  showErr(true, err_place, "Xahis edirik adivizi veya yasinizi girin");
+});
+
+addMany.addEventListener("click", function () {
+  let price = +prompt("Balansinizi artiramaq ucun qiymet daxil edin.");
+  atmData.increment(price);
+});
+
+payMany.addEventListener("click", function () {
+  let price = +prompt("Balansizinizda cixacaq meblegi yazin");
+
+  atmData.decrement(price);
+});
+
+showBalance.addEventListener("click", function () {
+  atmData.balance();
+});
+
+function showErr(bool, err_place, title) {
+  if (bool) {
+    err_place.innerHTML = title;
+  }
+  return false;
+}
